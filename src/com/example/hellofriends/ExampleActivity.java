b9260a7d3e1cc812299e8e9a84c4f64dd539e7ea
Sam Phippen <samphippen@googlemail.com>
2014-06-28 17:47:42 +0100

    initial commit

---
+++ src/com/example/hellofriends/ExampleActivity.java
@@ -0,0 +1,11 @@
+package com.example.hellofriends;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ExampleActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main_layout);
+	}
+}
